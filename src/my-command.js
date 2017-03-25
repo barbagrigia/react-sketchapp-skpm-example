7ea78473675ea34fb8849f50c9dbe727cb3ddc4c
Andrew Pouliot <andrew@darknoon.com>
2017-03-24 19:34:59 -0700

    wip

--- src/my-command.js
+++ src/my-command.js
@@ -1,6 +1,8 @@
+/* @flow */
 import React from 'react';
 import { render, Text, View } from 'react-sketchapp';
 import chroma from 'chroma-js';
+import fetch from 'sketch-module-fetch-polyfill';
 
 // take a hex and give us a nice text color to put over it
 const textColor = (hex) => {
@@ -61,20 +63,33 @@ const Document = ({ colors }) =>
   </View>;
 
 Document.propTypes = {
-  colors: React.PropTypes.arrayOf(React.PropTypes.shape(...Color)).isRequired,
+  colors: React.PropTypes.objectOf(React.PropTypes.string.isRequired).isRequired,
 };
 
 export default function (context) {
-  const colorList = {
-    Haus: '#F3F4F4',
-    Night: '#333',
-    Sur: '#96DBE4',
-    'Sur Dark': '#24828F',
-    Peach: '#EFADA0',
-    'Peach Dark': '#E37059',
-    Pear: '#93DAAB',
-    'Pear Dark': '#2E854B',
-  };
+  if (0) {
+    const colorList = {
+      Haus: '#F3F4F4',
+      Night: '#333',
+      Sur: '#96DBE4',
+      'Sur Dark': '#24828F',
+      Peach: '#EFADA0',
+      'Peach Dark': '#E37059',
+      Pear: '#93DAAB',
+      'Pear Dark': '#2E854B',
+    };
+    render(<Document colors={colorList} />, context);
+  } else {
+    const doc = context.document;
+    doc.showMessage("Loading JSON from github...");
+    const URL = "https://gist.githubusercontent.com/darknoon/b096646539f535ad205f6f845c6f0803/raw/efe8741c3c9fcde021ca05cf1c879ca99533ffe3/colors.json";
+    const colorList = fetch(URL)
+      .then( r => r.json() )
+      .then( colorList => {
+        render(<Document colors={colorList} />, context);
+        doc.hideMessage()
+      })
+      .catch( err => { doc.showMessage("" + err); log("ERROR:" + err);   coscript.setShouldKeepAround(false)} );
+  }
 
-  render(<Document colors={colorList} />, context);
 }
